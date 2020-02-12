40506696c291423d8931c09e3f9c3cc1d6d2ff8a
Giedrius <giedrius@blokas.io>
2020-02-12 14:30:56 +0000

    Removed -mtune=generic unrecognized argument

--- dpf/Makefile.base.mk
+++ dpf/Makefile.base.mk
@@ -129,7 +129,7 @@ endif
 # Set build and link flags
 
 BASE_FLAGS = -Wall -Wextra -pipe -MD -MP
-BASE_OPTS  = -O3 -ffast-math -mtune=generic -fdata-sections -ffunction-sections
+BASE_OPTS  = -O3 -ffast-math -fdata-sections -ffunction-sections
 
 ifeq ($(CPU_I386_OR_X86_64),true)
 BASE_OPTS += -msse -msse2
